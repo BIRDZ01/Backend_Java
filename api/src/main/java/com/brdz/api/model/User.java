96d41e2676bacf1ea91ad6832de26faf2df8f2dd
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2022-08-22 21:04:55 -0400

    completed user model

---
+++ api/src/main/java/com/brdz/api/model/User.java
@@ -0,0 +1,104 @@
+package com.brdz.api.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="bird_id")
+    private Integer id;
+
+    @Column(name="screen_name")
+    private String screenName;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="address")
+    private String address;
+
+    @Column(name="city")
+    private String city;
+
+//    TODO: Figure out how to make this work!!
+//    @Enumerated(EnumType.STRING)
+//    @Column(name="state")
+    private enum state{
+        AL,
+        AK,
+        AZ,
+        AR,
+        CA,
+        CO,
+        CT,
+        DE,
+        FL,
+        GA,
+        HI,
+        ID,
+        IL,
+        IN,
+        IA,
+        KS,
+        KY,
+        LA,
+        ME,
+        MD,
+        MA,
+        MI,
+        MN,
+        MS,
+        MO,
+        MT,
+        NE,
+        NV,
+        NH,
+        NJ,
+        NM,
+        NY,
+        NC,
+        ND,
+        OH,
+        OK,
+        OR,
+        PA,
+        RI,
+        SC,
+        SD,
+        TN,
+        TX,
+        UT,
+        VT,
+        VA,
+        WA,
+        WV,
+        WI,
+        WY
+    };
+
+    @Column(name="zipcode")
+    private Integer zipcode;
+
+    @Column(name="passcode")
+    private Integer passcode;
+
+    @Column(name="num_children")
+    private Integer numChildren;
+
+    @Column(name="er_contact_name")
+    private String erContactName;
+
+    @Column(name="er_contact_number")
+    private Integer erContactNumber;
+
+
+    public User(){}
+
+
+
+}
