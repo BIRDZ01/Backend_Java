23a61fd0955cdebb1ebfa61f2e5a83ccbd2663cc
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2022-08-22 21:15:44 -0400

    completed checkin model

---
+++ api/src/main/java/com/brdz/api/model/CheckIn.java
@@ -0,0 +1,31 @@
+package com.brdz.api.model;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Data
+public class CheckIn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="check_in_id")
+    private Integer id;
+
+    @Column(name="check_in_time")
+    private LocalDateTime check_in_time;
+
+//    TODO: Figure out how to make this work!
+//    @Column(name="result")
+    private enum result{
+        WELL,
+        OK,
+        BADLY
+};
+    public CheckIn(){}
+
+
+}
