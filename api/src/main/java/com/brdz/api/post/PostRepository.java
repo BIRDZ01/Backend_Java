fdf50c60514f915ab6479891213b721aa3f5ea88
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2022-08-29 20:41:00 -0400

    created PostRepository

---
+++ api/src/main/java/com/brdz/api/post/PostRepository.java
@@ -0,0 +1,11 @@
+package com.brdz.api.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+    List<Post> findPostById(Integer Id);
+}
