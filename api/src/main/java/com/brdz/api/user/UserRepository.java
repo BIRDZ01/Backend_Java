adeb568249201cf55688c1efe106d09d32bb5850
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2022-08-31 09:15:47 -0400

    created and populated UserRepository with 'findBy...' signatures.

--- api/src/main/java/com/brdz/api/user/UserRepository.java
+++ api/src/main/java/com/brdz/api/user/UserRepository.java
@@ -9,4 +9,11 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findUserById(Integer Id);
+    List<User> findUserByScreenName(String screenName);
+    List<User> findUserByName(String name);
+    List<User> findUserByAddress(String address);
+    List<User> findUserByCity(String city);
+    List<User> findUserByState(String state);
+    List<User> findUserByZipcode(Integer zipcode);
+    List<User> findUserByUsercode(Integer usercode);
 }
