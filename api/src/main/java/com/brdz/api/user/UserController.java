10e7e9f5624797ee05b0304e01d6c85957cef2c7
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2022-08-31 09:16:34 -0400

    created UserController and autowired UserRepository

--- api/src/main/java/com/brdz/api/user/UserController.java
+++ api/src/main/java/com/brdz/api/user/UserController.java
@@ -1,2 +1,16 @@
-package com.brdz.api.user;public class UserController {
+package com.brdz.api.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    //    CRUD methods
+
+    @Autowired
+    UserRepository userRepository;
+
+
+
 }
