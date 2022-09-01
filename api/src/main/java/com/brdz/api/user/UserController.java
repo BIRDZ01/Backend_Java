717144e8d4a1c15c904cc0f960bf603cd8d800c9
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2022-09-01 01:03:35 -0400

    started working on UserController getAllUsers method

--- api/src/main/java/com/brdz/api/user/UserController.java
+++ api/src/main/java/com/brdz/api/user/UserController.java
@@ -1,16 +1,29 @@
 package com.brdz.api.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     //    CRUD methods
 
-    @Autowired
-    UserRepository userRepository;
 
+
+
+//    Create
+
+//    Read
+//    List<User> getAll(){
+//        return repo.findAll();
+//    }
+//    Update
+
+//    Delete
 
 
 }
