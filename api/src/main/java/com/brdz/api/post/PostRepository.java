c1ab1339c007193e0179b8256ed16f9b9f6f1311
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2022-09-01 00:35:59 -0400

    completed findPostByBody and findPostByTime signatures

--- api/src/main/java/com/brdz/api/post/PostRepository.java
+++ api/src/main/java/com/brdz/api/post/PostRepository.java
@@ -3,9 +3,12 @@ package com.brdz.api.post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findPostById(Integer Id);
+    List<Post> findPostByBody(String postBody);
+    List<Post> findPostByTime(LocalDateTime postTime);
 }
