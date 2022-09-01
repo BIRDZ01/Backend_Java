e76632726294e050386f1d7c7400c11332f80c5f
NBryant83 <73191760+NBryant83@users.noreply.github.com>
2022-09-01 00:33:31 -0400

    completed findByTime and findByResult signatures

--- api/src/main/java/com/brdz/api/checkIn/CheckInRepository.java
+++ api/src/main/java/com/brdz/api/checkIn/CheckInRepository.java
@@ -3,9 +3,12 @@ package com.brdz.api.checkIn;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface CheckInRepository extends JpaRepository<CheckIn, Integer> {
     List<CheckIn> findCheckInById(Integer Id);
+    List<CheckIn> findCheckInByTime(LocalDateTime checkInTime);
+    List<CheckIn> findCheckInByResult(String result);
 }
